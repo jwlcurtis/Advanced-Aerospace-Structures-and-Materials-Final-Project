2d656b01c5dde2f74e1fa9f284c87cbdd1a98c4a
jwlcurtis <logantcb@yahoo.com>
2022-04-19 10:11:03 -0500

    Update Project_3.m

--- Project_3.m
+++ Project_3.m
@@ -1,5 +1,10 @@
 clc
 clear
-%% 
+%% Material Properties
+% properties=[Volume Fraction Modulus Denisty Poisson Rato]
+% units=[NA GPa g/cm^3 NA]
+F=[0.7 250 1.8 0.2]; % Fiber properties
+M=[0.3 3.5 1.2 0.35]; % Matrix properties
+Al=[1 72 2.78 0.33]; % Aluminum properties
 
-Al=[0.7 250 1.8 0.2];
+%% Design Problem 1a
